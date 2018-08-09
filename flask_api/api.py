f1145621573e7de5d1b27688c7cdb8a66f5547c9
lfreina <lfreinag@gmail.com>
2018-08-09 10:43:26 +0200

    Corrected the files, wrong project pushed before

--- flask_api/api.py
+++ flask_api/api.py
@@ -1,85 +1,117 @@
 from flask import Flask
-from flask_restful import Resource, Api
-from flask_restful import reqparse
 from utils import makeprediction
 
 from flask import render_template, flash, request
 from wtforms import Form, TextField, TextAreaField, validators, StringField,  SubmitField
 
-# App config.
-DEBUG = True
-
 app = Flask(__name__)
-
+app.debug = True
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
-#api = Api(app)
-
-
-
 class ReusableForm(Form):
-    slength = TextField('slength:', validators=[validators.required()])
-    swidth = TextField('swidth:', validators=[validators.required()])
-    plength = TextField('plength:', validators=[validators.required()])
-    pwidth = TextField('pwidth:', validators=[validators.required()])
-
+    # All suits 
+    suits1 = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
+    suits2 = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
+    suits3 = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
+    suits4 = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
+    suits5 = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
     
-# class Prediction(Resource):
-#     def get(self):
+    # Rank of card
+    rank1 = ['Ace', '2','3','4','5','6','7','8','9','10','Jack','Queen','King']
+    rank2 = ['Ace', '2','3','4','5','6','7','8','9','10','Jack','Queen','King']
+    rank3 = ['Ace', '2','3','4','5','6','7','8','9','10','Jack','Queen','King']
+    rank4 = ['Ace', '2','3','4','5','6','7','8','9','10','Jack','Queen','King']
+    rank5 = ['Ace', '2','3','4','5','6','7','8','9','10','Jack','Queen','King']
+def change_suit_to_number(option):
+    if option =='Clubs':
+        return 4
+    elif option =='Hearts':
+        return 1
+    elif option == 'Spades':
+        return 2
+    else:
+        return 3
 
-#         parser = reqparse.RequestParser()
-#         parser.add_argument('slength', type=float, help='slength cannot be converted')
-#         parser.add_argument('swidth', type=float, help='swidth cannot be converted')
-#         parser.add_argument('plength', type=float, help='plength cannot be converted')
-#         parser.add_argument('pwidth', type=float, help='pwidth cannot be converted')
-#         args = parser.parse_args()
+def change_rank_to_number(option):
+    if option =='Ace':
+        return 1
+    elif option =='Jack':
+        return 11
+    elif option =='Queen':
+        return 12
+    elif option =='King':
+        return 13
+    else:
+        return int(option)
 
-#         prediction = makeprediction.predict([
-#                 args['slength'], 
-#                 args['swidth'], 
-#                 args['plength'], 
-#                 args['pwidth']
-#             ])
 
-#         print "THE PREDICTION IS: " + str(prediction)
-
-#         return {
-#                 'slength': args['slength'],
-#                 'swidth': args['swidth'],
-#                 'plength': args['plength'],
-#                 'pwidth': args['pwidth'],
-#                 'species': prediction
-#                }
-
-@app.route("/", methods=['GET', 'POST'])
-def hello():
+@app.route('/', methods=['GET','POST'])    
+def dropdown():
+    # All suits and ranks structure
     form = ReusableForm(request.form)
-        
-    print form.errors
+    # Define the default values
+    if request.method == 'GET':
+        save =  { 'suits1':'Clubs',
+                  'rank1':'Ace',
+                  'suits2':'Spades',
+                  'rank2':'Ace',
+                  'suits3':'Hearts',
+                  'rank3':'Jack',
+                  'suits4':'Diamonds',
+                  'rank4':'Jack',
+                  'suits5':'Spades',
+                  'rank5':'Jack'}
+    
+    # Define what to do while sending with the button
     if request.method == 'POST':
-        slength=request.form['slength']
-        swidth=request.form['swidth']
-        plength=request.form['plength']
-        pwidth=request.form['pwidth']
- 
+        # Get the first card values
+        s1=request.form.get('suits1')
+        r1=request.form.get('rank1')
+        # Get the other card values
+        s2=request.form.get('suits2')
+        r2=request.form.get('rank2')
+        
+        s3=request.form.get('suits3')
+        r3=request.form.get('rank3')
+        
+        s4=request.form.get('suits4')
+        r4=request.form.get('rank4')
+        
+        s5=request.form.get('suits5')
+        r5=request.form.get('rank5')
+
+        # Save the card states
+        save =  { 'suits1':s1,
+                  'rank1':r1,
+                  'suits2':s2,
+                  'rank2':r2,
+                  'suits3':s3,
+                  'rank3':r3,
+                  'suits4':s4,
+                  'rank4':r4,
+                  'suits5':s5,
+                  'rank5':r5}
+
+        # Validate the entries
         if form.validate():
+            # Perform the prediction
             prediction = makeprediction.predict([
-                slength, 
-                swidth, 
-                plength, 
-                pwidth
+                change_suit_to_number(s1), change_rank_to_number(r1), 
+                change_suit_to_number(s2), change_rank_to_number(r2), 
+                change_suit_to_number(s3), change_rank_to_number(r3),
+                change_suit_to_number(s4), change_rank_to_number(r4),
+                change_suit_to_number(s5), change_rank_to_number(r5)
             ])
-              
-            # Save the comment here.
+
+            # Show the prediction
             flash('Prediction =  ' + prediction )
+            
         else:
             flash('All the form fields are required. ')
- 
-    return render_template('hello.html', form=form)
 
-    
-#api.add_resource(Prediction, '/prediction')
+    return render_template('test.html', form=form, save=save)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Run the app add port=# if you want to run on a different port than 5000
+if __name__ == "__main__":
+    app.run()
--- flask_api/utils/makeprediction.py
+++ flask_api/utils/makeprediction.py
@@ -1,23 +1,49 @@
-from sklearn import datasets
-from sklearn.neighbors import KNeighborsClassifier
+
 import numpy as np
-
 def predict(inputFeatures):
+    from keras.models import model_from_json
+    from keras import backend as K
 
-    iris = datasets.load_iris()
+    # Needed to clear all the variables
+    K.clear_session()
+    
+    # load json file and create model
+    json_file = open( 'model.json' , 'r' )
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    # load weights into new model
+    loaded_model.load_weights("model.h5")
 
-    knn = KNeighborsClassifier()
-    knn.fit(iris.data, iris.target)
+    # Transform inputFeatures
+    inputFeatures = np.array([inputFeatures])
 
-    inputFeat = np.array(inputFeatures)
-    inputFeatures = inputFeat.reshape(1,-1)
-    predictInt = knn.predict(inputFeatures)
-    if predictInt[0] == 0:
-        predictString = 'setosa'
-    elif predictInt[0] == 1:
-        predictString = 'versicolor'
-    elif predictInt[0] == 2:
-        predictString = 'virginica'
+    # Predict
+    prediction = loaded_model.predict(inputFeatures)
+
+    # Get the winning index
+    predictInt = np.argmax(prediction)
+    
+    if predictInt == 0:
+        predictString = 'Nothing in hand; not a recognized poker hand'
+    elif predictInt == 1:
+        predictString = 'One pair; one pair of equal ranks within five cards'
+    elif predictInt == 2:
+        predictString = 'Two pairs; two pairs of equal ranks within five cards'
+    elif predictInt == 3:
+        predictString = 'Three of a kind; three equal ranks within five cards'
+    elif predictInt == 4:
+        predictString = 'Straight; five cards sequentially ranked with no gaps'
+    elif predictInt == 5:
+        predictString = 'Flush; five cards with the same suit'
+    elif predictInt == 6:
+        predictString = 'Full house; pair + different rank three of a kind'
+    elif predictInt == 7:
+        predictString = 'Four of a kind; four equal ranks within five cards'
+    elif predictInt == 8:
+        predictString = 'Straight flush; straight + flush'
+    elif predictInt == 9:
+        predictString = 'Royal flush; {Ace, King, Queen, Jack, Ten} + flush'
     else:
         predictString = 'null'
 
